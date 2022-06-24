df5655682cc3b8bdd0b7b8260c3e630001373dfe
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-24 16:29:08 +0700

    create

---
+++ demo/src/main/java/com/example/demo/SpringsecurityloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityloginApplication.class, args);
+	}
+
+}
