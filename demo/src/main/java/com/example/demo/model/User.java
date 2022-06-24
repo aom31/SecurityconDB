6be6ff52094d3a874f89908dd1a2c097a3b1a5a3
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-24 17:33:39 +0700

    finish model

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -22,5 +22,9 @@ public class User {
     private  String lastName;
     private  String email;
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+
     private Collection<Role> roles;
 }
